96aa648c7db3555b5fe061b53bec3de3bb236980
Zejun Zhang <zzj19980310@gmail.com>
2023-04-25 15:27:49 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_parquet('hdfs:/user/bm106_nyu_edu/1004-project-2023/interactions_train_small.parquet')
+df = pd.read_parquet('/user/bm106_nyu_edu/1004-project-2023/interactions_train.parquet')
 
 print(df.head(10))

e12db541a0ffe3a03e300e571d268e5584b10053
Zejun Zhang <zzj19980310@gmail.com>
2023-05-14 11:06:07 -0400

    Update Indexer.py

--- ALS_model/Indexer.py
+++ ALS_model/Indexer.py
@@ -11,19 +11,19 @@ def main(spark):
     df = spark.read.parquet("hdfs:/user/zz4140_nyu_edu/interactions_train_small_80.parquet")
 
     # Use StringIndexer to convert string to numeric
-    indexer_recording = StringIndexer(inputCol="recording_msid", outputCol="recording_msid_index")
-    df = indexer_recording.fit(df).transform(df)
+    #indexer_recording = StringIndexer(inputCol="recording_msid", outputCol="recording_msid_index")
+    #df = indexer_recording.fit(df).transform(df)
 
     # Drop the timestamp column
     df = df.drop("timestamp")
 
     # Count the number of times a user has listened to a song
-    df_count = df.groupBy("user_id", "recording_msid_index").count()
-    df_count = df_count.withColumnRenamed("count", "count_combination")
+    #df_count = df.groupBy("user_id", "recording_msid_index").count()
+    #df_count = df_count.withColumnRenamed("count", "count_combination")
 
     df.write.parquet("indexed_train_small.parquet")
 
-    #return df_count
+    return df
     spark.stop()
 
 if __name__ == "__main__":
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     # Get file_path for dataset to analyze
     #parquet_file_path = sys.argv[1]
 
-    main(spark)
+    df = main(spark)

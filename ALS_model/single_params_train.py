b0e1769edec9b1d76dbb5c670a10fed180412390
Zejun Zhang <zzj19980310@gmail.com>
2023-05-16 15:01:33 -0400

    Update single_params_train.py

--- ALS_model/single_params_train.py
+++ ALS_model/single_params_train.py
@@ -41,8 +41,8 @@ def main(spark, train_path, val_path):
     true_tracks = val.select('user_id', 'recording_idx').orderBy('user_id',"count",ascending=False).groupBy('user_id').agg(expr('collect_list(recording_idx) as tracks'))
 
     rank_val =  50 #default is 10
-    reg_val =  0.005  #default is 1
-    alpha_val = 0.01 #default is 1
+    reg_val =  0.05  #default is 1
+    alpha_val = 1 #default is 1
 
     # maps=[]
     # precs=[]

5d771e70189d9c70846de8ed0e514ebcc424674e
robinwang9 <112658163+robinwang9@users.noreply.github.com>
2023-05-16 20:00:10 -0400

    Update LightFM.py

--- LightFM/LightFM.py
+++ LightFM/LightFM.py
@@ -74,7 +74,7 @@ def train_evaluate_model(loss, no_components, user_alpha, interactions_train, we
     val_precision = precision_at_k(model, interactions_val, k=100).mean()
     return val_precision
 # Parameter search
-ranks = [20, 30, 40]
+ranks = [20, 30, 40, 50]
 regularizations = [0.005, 0.01, 0.1]
 
 # Add a dictionary to store precision at k scores for all parameter combinations
